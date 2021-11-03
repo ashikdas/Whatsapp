ff61b0853c7158234e440c0f1444fec410eb60d3
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-11-04 01:23:56 +0600

    all test case complete

--- Whatsapp/src/test/java/WhatsappUITest/UITestNG.java
+++ Whatsapp/src/test/java/WhatsappUITest/UITestNG.java
@@ -1,13 +1,13 @@
 package WhatsappUITest;
 
+import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.ss.usermodel.CellBase;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -19,6 +19,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 
@@ -36,7 +37,7 @@ public class UITestNG {
 		driver.get("https://web.whatsapp.com/");
 		Thread.sleep(15000);
 	}
-	@Test
+	@Test (priority=1)
 	public void verifySearchNumber() throws Exception{
 		
 		// File object create & location the xlsx file
@@ -51,7 +52,7 @@ public class UITestNG {
 				
 		XSSFSheet sheet= xsf.getSheetAt(0);
 				
-		String data =sheet.getRow(0).getCell(0).getStringCellValue();
+//		String data =sheet.getRow(0).getCell(0).getStringCellValue();
 		
 //		System.out.println(new DataFormatter().formatCellValue(sheet.getRow(1).getCell(0)));
 		
@@ -65,7 +66,9 @@ public class UITestNG {
 		rb.keyPress(KeyEvent.VK_ENTER);
 		Thread.sleep(3000);
 		rb.keyRelease(KeyEvent.VK_ENTER);
+		xsf.close();
 		
+		/*
 		// Test Case 2
 		UITestObject.writeMessage(driver).sendKeys("Robot send message");
 //		Thread.sleep(3000);
@@ -89,12 +92,131 @@ public class UITestNG {
 		
 		// Test Case 4
 		
-		UITestObject.messageStatus(driver).getText();
+		Thread.sleep(5000);
+		String result= UITestObject.messageStatus(driver).getAttribute("aria-label");
 		
-		System.out.println("Output" + UITestObject.messageStatus(driver).getText());
+		XSSFCell cell = sheet.getRow(1).createCell(2);
 		
-		driver.quit();
+		cell.setCellValue(result);
 		
+		FileOutputStream outputStream = new FileOutputStream("D:\\SQA Projects\\QUPS\\Whatsapp\\Numbers.xlsx");
+		xsf.write(outputStream);
+		
+		xsf.close();
+		
+
+		// Test Case 5
+		
+		UITestObject.menu(driver).click();
+		
+		rb.keyPress(KeyEvent.VK_TAB);
+		rb.keyRelease(KeyEvent.VK_TAB);
+		
+		Thread.sleep(3000);
+		rb.keyPress(KeyEvent.VK_UP);
+		rb.keyRelease(KeyEvent.VK_UP);
+		
+		Thread.sleep(2000);
+		rb.keyPress(KeyEvent.VK_ENTER);
+		
+		Thread.sleep(5000); */
+		
+//		driver.quit();
+		
+	}
+	@Test (priority=2)
+	public void verifySendMessage() throws Exception{
+		
+		UITestObject.writeMessage(driver).sendKeys("Robot send message");
+		Thread.sleep(3000);
+		
+		Robot sendKey = new Robot();
+		sendKey.keyPress(KeyEvent.VK_ENTER);
+		Thread.sleep(3000);
+		
+	}
+	
+	@Test (priority=3)
+	public void verifySuccessfullySendMessage() throws Exception{
+		
+		File src=new File("D:\\SQA Projects\\QUPS\\Whatsapp\\Numbers.xlsx");
+		
+		// File input stream class used for read & write action from the file & pass the file object as a parameter
+				
+		FileInputStream fis=new FileInputStream(src);
+				
+		XSSFWorkbook xsf= new XSSFWorkbook(fis);
+				
+		XSSFSheet sheet= xsf.getSheetAt(0);
+		
+		XSSFCell cell = sheet.getRow(1).createCell(1);
+		
+		cell.setCellValue("Sent");
+		
+		FileOutputStream outputStream = new FileOutputStream("D:\\SQA Projects\\QUPS\\Whatsapp\\Numbers.xlsx");
+		xsf.write(outputStream);
+		
+		xsf.close();
+	}
+	
+	@Test (priority=4)
+	public void verifySendMessageStatus() throws Exception{
+	
+		Thread.sleep(5000);
+		File src=new File("D:\\SQA Projects\\QUPS\\Whatsapp\\Numbers.xlsx");
+		
+		// File input stream class used for read & write action from the file & pass the file object as a parameter
+				
+		FileInputStream fis=new FileInputStream(src);
+				
+		XSSFWorkbook xsf= new XSSFWorkbook(fis);
+				
+		XSSFSheet sheet= xsf.getSheetAt(0);
+		
+		String result= UITestObject.messageStatus(driver).getAttribute("aria-label");
+		
+		if (result =="Read"){
+			
+			result = "seen";
+		}
+		else{
+			result = "not seen";
+		}
+		
+		XSSFCell cell = sheet.getRow(1).createCell(2);
+		
+		cell.setCellValue(result);
+		
+		FileOutputStream outputStream = new FileOutputStream("D:\\SQA Projects\\QUPS\\Whatsapp\\Numbers.xlsx");
+		xsf.write(outputStream);
+		
+		xsf.close();
+	
+	}
+	
+	@Test (priority=5)
+	public void verifyLogout() throws Exception{
+		
+		UITestObject.menu(driver).click();
+		Robot rb = new Robot();
+		rb.keyPress(KeyEvent.VK_TAB);
+		rb.keyRelease(KeyEvent.VK_TAB);
+		
+		Thread.sleep(3000);
+		rb.keyPress(KeyEvent.VK_UP);
+		rb.keyRelease(KeyEvent.VK_UP);
+		
+		Thread.sleep(2000);
+		rb.keyPress(KeyEvent.VK_ENTER);
+		
+		Thread.sleep(5000);
+	}
+	
+	@AfterTest
+	public static void Teardown()
+	{
+		driver.close();
+		System.out.println("Testing end");
 	}
 
 }
--- Whatsapp/src/test/java/WhatsappUITest/UITestObject.java
+++ Whatsapp/src/test/java/WhatsappUITest/UITestObject.java
@@ -1,7 +1,12 @@
 package WhatsappUITest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,7 +14,6 @@ import org.openqa.selenium.WebElement;
 public class UITestObject {
 	
 private static WebElement locator = null;
-private static List<WebElement> locators = null;
 	
 	public static WebElement searchBox(WebDriver driver){
 		locator = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/label[1]/div[1]/div[2]"));
@@ -22,14 +26,13 @@ private static List<WebElement> locators = null;
 	}
 	
 	public static WebElement messageStatus(WebDriver driver){
-//		locators = driver.findElements(By.xpath("//body[@class='web']/div[@id='app']/div[@class='_1ADa8 _3Nsgw app-wrapper-web os-win']/div[@class='_1XkO3 two']/div[@class='ldL67 _3sh5K']/div[@id='main']/div[@class='_1LcQK']/div[@class='tm2tP copyable-area']/div[@class='_33LGR']/div[@class='y8WcF']/div[20]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]"));
-		locator = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[3]/div[1]/div[2]/div[3]/div[22]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]/*"));
+		locator = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[3]/div[1]/div[2]/div[3]/div[23]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]"));
 		return locator;
 	}
 
+	public static WebElement menu(WebDriver driver){
+		locator = driver.findElement(By.xpath("//span[@data-testid='menu']"));
+		return locator;
+	}
 }
-//body[@class='web']/div[@id='app']/div[@class='_1ADa8 _3Nsgw app-wrapper-web os-win']/div[@class='_1XkO3 two']/div[@class='ldL67 _3sh5K']/div[@id='main']/div[@class='_1LcQK']/div[@class='tm2tP copyable-area']/div[@class='_33LGR']/div[@class='y8WcF']/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]
-//body[@class='web']/div[@id='app']/div[@class='_1ADa8 _3Nsgw app-wrapper-web os-win']/div[@class='_1XkO3 two']/div[@class='ldL67 _3sh5K']/div[@id='main']/div[@class='_1LcQK']/div[@class='tm2tP copyable-area']/div[@class='_33LGR']/div[@class='y8WcF']/div[17]/div[1]/div[1]/div[1]/div[1]/div[1]
-//body[@class='web']/div[@id='app']/div[@class='_1ADa8 _3Nsgw app-wrapper-web os-win']/div[@class='_1XkO3 two']/div[@class='ldL67 _3sh5K']/div[@id='main']/div[@class='_1LcQK']/div[@class='tm2tP copyable-area']/div[@class='_33LGR']/div[@class='y8WcF']/div[16]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]
-//body[@class='web']/div[@id='app']/div[@class='_1ADa8 _3Nsgw app-wrapper-web os-win']/div[@class='_1XkO3 two']/div[@class='ldL67 _3sh5K']/div[@id='main']/div[@class='_1LcQK']/div[@class='tm2tP copyable-area']/div[@class='_33LGR']/div[@class='y8WcF']/div[17]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]
-//body[@class='web']/div[@id='app']/div[@class='_1ADa8 _3Nsgw app-wrapper-web os-win']/div[@class='_1XkO3 two']/div[@class='ldL67 _3sh5K']/div[@id='main']/div[@class='_1LcQK']/div[@class='tm2tP copyable-area']/div[@class='_33LGR']/div[@class='y8WcF']/div[20]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]
+
